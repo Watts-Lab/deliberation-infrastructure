90dfe7da75a169ac990a71fa2d3d426342c60489
kepstein23 <97930010+kepstein23@users.noreply.github.com>
2023-06-22 13:13:41 -0400

    add outputs to deliberation-infrastructure, create security group for efs

---
+++ deliberation-empirica/securityGroup.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "efs_sg" {
+  name        = "${var.app_name}-efs-sg"
+  description = "${var.app_name} EFS security group"
+  vpc_id      = aws_vpc.project_vpc.id
+
+  ingress {
+    description     = "Allow only from fargate service"
+    from_port       = 2049
+    to_port         = 2049
+    protocol        = "tcp"
+    security_groups = [aws_security_group.app_ecs_task_sg.id]
+  }
+
+  # Only ingress rules added
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
