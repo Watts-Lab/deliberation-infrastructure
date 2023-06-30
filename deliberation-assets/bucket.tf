caf37b2c1061f9be54331edf64f5d14dab2521d7
kepstein23 <97930010+kepstein23@users.noreply.github.com>
2023-06-30 13:22:31 -0400

    create bucket.tf

---
+++ deliberation-assets/bucket.tf
@@ -0,0 +1,17 @@
+//make new s3 bucket
+resource "aws_s3_bucket" "deliberation-assets-bucket"{
+  bucket = "deliberation-assets"
+
+  tags = {
+    deployBy = "terraform"
+    app = "deliberation-assets"
+  }
+}
+
+//enable versioning to prevent overwriting or deletion and to archive previous versions
+resource "aws_s3_bucket_versioning" "versioning-deliberation-assets" {
+  bucket = aws_s3_bucket.deliberation-assets-bucket.bucket
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
