92c2f82f487e5132361d6732a092db11464a4dc9
kepstein23 <97930010+kepstein23@users.noreply.github.com>
2023-07-03 12:59:18 -0400

    add website configuration

--- deliberation-assets/bucket.tf
+++ deliberation-assets/bucket.tf
@@ -1,6 +1,9 @@
 //make new s3 bucket
-resource "aws_s3_bucket" "deliberation-assets-bucket"{
+resource "aws_s3_bucket_website_configuration" "deliberation-assets-bucket"{
   bucket = var.bucket_name
+  index_document {
+    suffix = "README.md"
+  }
 }
 
 //enable versioning to prevent overwriting or deletion and to archive previous versions
--- deliberation-assets/route53.tf
+++ deliberation-assets/route53.tf
@@ -5,11 +5,11 @@ data "aws_route53_zone" "selected" {
 
 resource "aws_route53_record" "assets" {
   zone_id = data.aws_route53_zone.selected.zone_id
-  name    = "assets"
+  name    = var.bucket_name
   type    = "A"
   alias {
-    name                   = aws_s3_bucket.deliberation-assets-bucket.website_domain
-    zone_id                = aws_s3_bucket.deliberation-assets-bucket.hosted_zone_id
+    name                   = aws_s3_bucket_website_configuration.deliberation-assets-bucket.website_domain
+    zone_id                = aws_s3_bucket_website_configuration.deliberation-assets-bucket.hosted_zone_id
     evaluate_target_health = true
   }
 }
