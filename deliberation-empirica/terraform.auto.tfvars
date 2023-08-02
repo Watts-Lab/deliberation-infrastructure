546d81b34bdc175ac7953415806a99505d68b432
James Houghton <james.p.houghton@gmail.com>
2023-08-02 10:19:30 -0400

    restructure paths

--- deliberation-empirica/terraform.auto.tfvars
+++ deliberation-empirica/terraform.auto.tfvars
@@ -6,7 +6,5 @@ app_data_path        = "/data"
 
 # app environment variables
 QUALTRICS_DATACENTER    = "iad1"
-GITHUB_DATA_REPO        = "Watts-Lab/deliberation-data-private"
-GITHUB_BRANCH           = "main"
 REACT_APP_TEST_CONTROLS = "disabled"
 
--- deliberation-empirica/variables.tf
+++ deliberation-empirica/variables.tf
@@ -2,21 +2,21 @@
 variable "app_name" {}
 variable "org_name" {} 
 variable "shared_workspace_name" {}
-variable "region" {}
 
-variable "container_image_name"{
-  default = "ghcr.io/watts-lab/deliberation-empirica:latest"
-}
-# app environment variables
-variable "QUALTRICS_DATACENTER" {}
-variable "GITHUB_DATA_REPO" {}
-variable "GITHUB_BRANCH" {}
-variable "REACT_APP_TEST_CONTROLS" {}
 variable "app_data_path" {
   type        = string
   description = "Path to app data"
   default     = "/data"
 }
+variable "deliberation_empirica_tag" {
+  type        = string
+  description = "Docker image tag for deliberation-empirica"
+}
+
+# app environment variables
+variable "QUALTRICS_DATACENTER" {}
+variable "REACT_APP_TEST_CONTROLS" {}
+
 
 # taken from TF_VAR_* environment variables or set on TF cloud
 variable "DAILY_APIKEY" {
@@ -31,6 +31,8 @@ variable "DELIBERATION_MACHINE_USER_TOKEN" {
 variable "EMPIRICA_ADMIN_PW" {
   sensitive = true
 }
+
+variable "region" {}
 
 
 
